26e18133d6b01c7b23440f65d629fafd399e10ce
suptalentdev <suptalentdev@gmail.com>
2016-02-04 21:28:33 +0000

    periodic checkin

--- src/collection/mod.rs
+++ src/collection/mod.rs
@@ -8,12 +8,13 @@ pub mod least_upper_bound;
 pub mod lookup;
 pub mod trace;
 pub mod compact;
-pub mod tier;
+pub mod trie;
 pub mod count;
 pub mod robin_hood;
+pub mod basic;
 
 pub use collection::lookup::Lookup;
 pub use collection::least_upper_bound::LeastUpperBound;
 pub use collection::least_upper_bound::close_under_lub;
-pub use collection::trace::Trace;
-pub use collection::trace::Offset;
+pub use collection::trace::{Trace, TraceRef};
+pub use collection::basic::{BasicTrace, Offset};
