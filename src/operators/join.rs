5aed5ea1ba2d9b61566e10a65ac2214738bc190a
suptalentdev <suptalentdev@gmail.com>
2020-12-28 18:12:13 -0500

    change Fn constraint to FnMut

--- src/operators/join.rs
+++ src/operators/join.rs
@@ -28,9 +28,7 @@ use trace::TraceReader;
 /// Join implementations for `(key,val)` data.
 pub trait Join<G: Scope, K: Data, V: Data, R: Semigroup> {
 
-    /// Matches pairs `(key,val1)` and `(key,val2)` based on `key` and yields pairs `(key, (val1, val2))`.
-    ///
-    /// The [`join_map`](Join::join_map) method may be more convenient for non-trivial processing pipelines.
+    /// Matches pairs `(key,val1)` and `(key,val2)` based on `key` and then applies a function.
     ///
     /// # Examples
     ///
